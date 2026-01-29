2acd1d84a6beb2636aba858b5bc09d96e00e5675
Deslin Delvi <deslindelvi7@gmail.com>
2026-01-29 14:33:09 +0530

    Improved frontend UI for viewing on smartphones

--- offline_predict.py
+++
@@ -1,6 +0,0 @@
-from ultralytics import YOLO
-
-model = YOLO("models/best.pt")
-
-# Run detection on all images in the folder and save annotated outputs
-model.predict(source="test_images", save=True)
